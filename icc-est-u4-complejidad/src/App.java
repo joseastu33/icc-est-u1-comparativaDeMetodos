085ae99da7b6c1f5df9e259eeb4970777caf99ac
Jose <joseastu3345@gmail.com>
2026-04-14 23:41:42 -0500

    Actualizacion

---
+++ icc-est-u4-complejidad/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Este es un programa en Java");
+        int array[] = new int[] {1,2,3,4,5};
+        System.out.println(array[0]);
+        System.out.println(array); //-- imprime un OBJETO, no el ARREGLO
+        //ciclo para IMPRIMIR el ARREGLO
+        // for normal
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+        //for each
+        for (int i: array){
+            System.out.println(i);
+        }
+        /* archivo en verde: preparado para hacer un commit(GUARDAR)
+         archivo en amarillo: ya existe en Git, pero ha sido modificado */
+    }
+}
